af4a6b4154141cb9006ce63b9971b79fa761188f
AlexGreen546 <voxelin@gmail.com>
2021-10-21 00:16:06 +0300

    Add second task

---
+++ Task 2/Task 2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Int32.Parse(ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int CountNumber;
+                CountNumber = 0;
+                for (int y = (int)Pow(i, 2.0); y > 10; y /= 10)
+                {
+                    CountNumber++;
+                }
+                if (i == Pow(i, 2) % Pow(10, CountNumber))
+                {
+                    WriteLine(i);
+                }
+
+            }
+
+        }
+    }
+}
